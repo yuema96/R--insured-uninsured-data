d4531ed4c5ea63797acf441eb89addce14db8839
Yue Ma <57729954+yuema96@users.noreply.github.com>
2023-10-24 08:37:28 -0500

    Add files via upload

---
+++ uninsured.r
@@ -0,0 +1,5 @@
+# Part 1: Calculate and plot the percentage of uninsured individuals based on data in `uninsured2012.csv`; your visualization should convey the percentage of uninsured individuals for each state. 
+
+# Part 2: Sort the states by population, where the most populated state is listed first. Again plot the percentage of uninsured individuals for each state.
+
+# Part 3: Using both the `uninsured2012.csv` and `regions.csv` datasets, produce a visualization that encodes the insured and uninsured by each region.
